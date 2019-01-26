59d2d8c8a18f11811b682d5149d8b294fa9b6ab5
Juntao Zhu <juzhu@microsoft.com>
2019-01-26 21:27:58 +0800

    init

---
+++ LabHighCpuCoreSite/Controllers/ReproController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LabHighCpuCoreSite.Controllers
+{
+    public class ReproController : Controller
+    {
+        static volatile bool enabled = true;
+
+        public string Enable()
+        {
+            enabled = true;
+            return "Done";
+        }
+
+        public string Disable()
+        {
+            enabled = false;
+            return "Done";
+        }
+
+        public string Index()
+        {
+            int i = DoJob();
+            return i.ToString();
+        }
+
+        private int DoJob()
+        {
+            int i = 0;
+            while (enabled)
+            {
+                for (int j = 0; j < 1000; j++) ;
+
+                Thread.Yield();
+                i++;
+            }
+            return i;
+        }
+    }
+}
---
+++ LabHighCpuCoreSite/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace LabHighCpuCoreSite.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
